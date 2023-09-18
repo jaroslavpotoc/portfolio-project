191f239ffa9d198a135ea56bd933d02876332c34
jaroslavpotoc <jaroslavpotocny@icloud.com>
2023-09-18 20:59:28 +0200

    add some ASCII art

--- message-generator-program/messageGeneratorProgram.js
+++ message-generator-program/messageGeneratorProgram.js
@@ -1,34 +1,65 @@
-// Generates a random number between 0 and num - 1
 function generateRandomNumber(num) {
-    return Math.floor(Math.random() * num);
-  }
-  
-  // Array that stores random messages
-  const randomMessages = [
-    "Today's motivation: It's never too late to start anew.",
-    "Did you know that bees communicate through dancing? 🐝",
-    "Quote of the day: 'Happiness is not something ready made. It comes from your own actions.' - Dalai Lama",
-    "Why do you find books in libraries? Because they're there!",
-    "On this day in 1969, humans first landed on the Moon.",
-    "Today is the right day to start a new adventure.",
-    "Equations are like puzzles for mathematicians. Here's one: 2x + 3 = 7. What is the value of x?",
-    "What is the tallest mountain in the world?",
-    "We recommend the song 'Imagine' by John Lennon today.",
-    "Can you guess which continent has the most inhabitants?"
-  ];
-  
-  // Generates a random message from the provided messages array
-  function generateRandomMessage(messages) {
-    const randomIndex = generateRandomNumber(messages.length);
-    return messages[randomIndex];
-  }
-  
-  // Formats and displays a random message
-  function formatWisdom() {
-    const randomMessage = generateRandomMessage(randomMessages);
-    console.log(randomMessage);
-  }
-  
-  // Call the formatWisdom function to display a random message
-  formatWisdom();
-  
+  // Gets a number from 0 to num - 1
+  return Math.floor(Math.random() * num);
+}
+
+const randomMessages = [
+  "Today's motivation:\n"
+  + "   _____          __  __ ______    ______      ________ _____  \n"
+  + "  / ____|   /\\   |  \\/  |  ____|  / __ \\ \\    / /  ____|  __ \\ \n"
+  + " | |  __   /  \\  | \\  / | |__    | |  | \\ \\  / /| |__  | |__) |\n"
+  + " | | |_ | / /\\ \\ | |\\/| |  __|   | |  | |\\ \\/ / |  __| |  _  / \n"
+  + " | |__| |/ ____ \\| |  | | |____  | |__| | \\  /  | |____| | \\ \\ \n"
+  + "  \\_____/_/    \\_\\_|  |_|______|  \\____/   \\/   |______|_|  \\_\\",
+  "Did you know that bees communicate through dancing? 🐝",
+  "Quote of the day:\n"
+  + "  _______ _                 _     \n"
+  + " |__   __| |               | |    \n"
+  + "    | |  | |__   __ _ _ __ | | __ \n"
+  + "    | |  | '_ \\ / _` | '_ \\| |/ / \n"
+  + "    | |  | | | | (_| | | | |   <  \n"
+  + "    |_|  |_| |_|\\__,_|_| |_|_|\\_\\",
+  "Why do you find books in libraries?\n"
+  + "  ____  _ _ _     ____ _ _ _ _\n"
+  + " |  _ \\(_) | |   |  _ \\ (_) | | |\n"
+  + " | |_) |_| | |   | |_) |_| | | |\n"
+  + " |  _ <| | | |   |  _ <| | | | |\n"
+  + " | |_) | | | |___| |_) | | | | |\n"
+  + " |____/|_|_|_____|____/|_|_|_|_|",
+  "On this day in 1969, humans first landed on the Moon.\n"
+  + "  _    _ _   _ _ _ _   _ _ _ _ _ _\n"
+  + " / \\  /  / |  _ _ _ _  |  _ _ _ _ _|\n"
+  + "|   ||  |  | |       | | |          \n"
+  + "|   ||  |  | |       | | |  _ _ _ _\n"
+  + " \\_/  \\_/  |_|_ _ _ _| |_|_ _ _ _ _|",
+  "Today is the right day to start a new adventure.",
+  "Equations are like puzzles for mathematicians:\n"
+  + "  _____ _     _        _  _____ \n"
+  + " / ____| |   (_)      | |/ ____|\n"
+  + "| |    | |__  _  ___  | | |  __ \n"
+  + "| |    | '_ \\| |/ _ \\ | | | |_ |\n"
+  + "| |____| | | | |  __/ | | |__| |\n"
+  + " \\_____|_| |_|_|\\___| |_|\\_____|\n"
+  + "                                ",
+  "What is the tallest mountain in the world?\n"
+  + "  __   __  _ _ _ _ _ _ _ _ _ _  \n"
+  + "  \\ \\ / / |  _ _ _ _ _ _ _ _ _| \n"
+  + "   \\ V /  | |                      \n"
+  + "    > <   | | _ _ _ _ _ _ _   \n"
+  + "   / . \\  | ||                            \n"
+  + "  /_/ \\_\\ |_| _ _ _ _ _ _ _ _|",
+  "We recommend the song 'Imagine' by John Lennon today.",
+  "Can you guess which continent has the most inhabitants?"
+];
+
+function generateRandomMessage(messages) {
+  const randomIndex = generateRandomNumber(messages.length);
+  return messages[randomIndex];
+}
+
+function formatWisdom() {
+  const randomMessage = generateRandomMessage(randomMessages);
+  console.log(randomMessage);
+}
+
+formatWisdom();
