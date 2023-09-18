5d840921ccbfda26dd878676807b982263b27771
jaroslavpotoc <jaroslavpotocny@icloud.com>
2023-09-18 20:47:21 +0200

    Create generate random number and arrray message

--- message-generator-program/messageGeneratorProgram.js
+++ message-generator-program/messageGeneratorProgram.js
@@ -1,1 +1,18 @@
-console.log("Hello word!")
+// Generates a random number between 0 and num - 1
+function generateRandomNumber(num) {
+    return Math.floor(Math.random() * num);
+  }
+  
+// Array that stores random messages
+const randomMessage = [
+    "Today's motivation: It's never too late to start anew.",
+    "Did you know that bees communicate through dancing? 🐝",
+    "Quote of the day: 'Happiness is not something ready made. It comes from your own actions.' - Dalai Lama",
+    "Why do you find books in libraries? Because they're there!",
+    "On this day in 1969, humans first landed on the Moon.",
+    "Today is the right day to start a new adventure.",
+    "Equations are like puzzles for mathematicians. Here's one: 2x + 3 = 7. What is the value of x?",
+    "What is the tallest mountain in the world?",
+    "We recommend the song 'Imagine' by John Lennon today.",
+    "Can you guess which continent has the most inhabitants?"
+];
